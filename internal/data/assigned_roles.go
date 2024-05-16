a7b8c169589589722d21c116269328c6aeecee20
Aidar Kudabaev <a.kudabaev@calendaria.ai>
2024-05-16 17:27:17 +0500

    update roles

--- internal/data/assigned_roles.go
+++ internal/data/assigned_roles.go
@@ -77,7 +77,7 @@ func (t *assignedRolesRepo) ListAssignedRoles(ctx context.Context, dto ListRoles
 		query.Where(
 			teamidentityrole.Or(
 				teamidentityrole.TeamIDIn(dto.TeamsIDs...),
-				teamidentityrole.TeamIDIsNil(),
+				teamidentityrole.TeamIDIsNil(), //TODO check if that's correct
 			),
 		)
 	}
