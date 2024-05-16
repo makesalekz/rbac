a3c74e181d4181a5e98b0dae59235873e54aec44
Aidar Kudabaev <a.kudabaev@calendaria.ai>
2024-05-16 11:53:30 +0500

    deleted duplicate role

--- ent/migrate/migrations/20240424065635_init.sql
+++ ent/migrate/migrations/20240424065635_init.sql
@@ -16,12 +16,11 @@ INSERT INTO "permissions" ("id", "group_id", "name", "description", "app_id", "f
 
 -- Create project roles
 INSERT INTO "roles" ("id", "name", "description", "is_system", "created_at", "updated_at") VALUES
-  (3, 'Project Owner', 'Has all permissions of a project owner', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-  (4, 'Project Manager', 'Has all permissions of a project manager', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-  (5, 'Project Participant', 'Has all limitations of project participant', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-  (6, 'Project Viewer', 'Has all limitations of project viewer', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  (3, 'Project Manager', 'Has all permissions of a project manager', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  (4, 'Project Participant', 'Has all limitations of project participant', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  (5, 'Project Viewer', 'Has all limitations of project viewer', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 
-  (7, 'Direct Participant', 'Has all permissions of a Direct Participant', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+  (6, 'Direct Participant', 'Has all permissions of a Direct Participant', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 
 -- Create permissions for basic roles
@@ -35,34 +34,24 @@ INSERT INTO "role_permissions" ("role_id", "permission_id", "deny", "fields") VA
   (1, 'project.team.assign', FALSE, '[]'),
   (1, 'project.attachments.create',FALSE,'[]'),
   (1, 'project.attachments.delete',FALSE,'[]'),
--- Project owner role
+-- Project manager role
   (3, 'admin.team.read', FALSE, '[]'),
   (3, 'project.project.create', FALSE, '[]'),
   (3, 'project.project.read', FALSE, '[]'),
   (3, 'project.project.update', FALSE, '[]'),
   (3, 'project.project.delete', FALSE, '[]'),
-  (3, 'project.project.manager', FALSE, '[]'),
   (3, 'project.team.assign', FALSE, '[]'),
   (3, 'project.attachments.create',FALSE,'[]'),
   (3, 'project.attachments.delete',FALSE,'[]'),
--- Project manager role
-  (4, 'admin.team.read', FALSE, '[]'),
+-- Project participant limitations
   (4, 'project.project.create', FALSE, '[]'),
   (4, 'project.project.read', FALSE, '[]'),
-  (4, 'project.project.update', FALSE, '[]'),
-  (4, 'project.project.delete', FALSE, '[]'),
-  (4, 'project.team.assign', FALSE, '[]'),
   (4, 'project.attachments.create',FALSE,'[]'),
   (4, 'project.attachments.delete',FALSE,'[]'),
--- Project participant limitations
+-- Project reporter limitations
   (5, 'project.project.create', FALSE, '[]'),
   (5, 'project.project.read', FALSE, '[]'),
-  (5, 'project.attachments.create',FALSE,'[]'),
-  (5, 'project.attachments.delete',FALSE,'[]'),
--- Project reporter limitations
-  (6, 'project.project.create', FALSE, '[]'),
-  (6, 'project.project.read', FALSE, '[]'),
 
 -- Direct particiant limitation
-  (7, 'project.project.read', TRUE, '[]'),
-  (7, 'project.project.create', FALSE, '[]');
+  (6, 'project.project.read', TRUE, '[]'),
+  (6, 'project.project.create', FALSE, '[]');
