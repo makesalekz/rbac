ac554d6f364fdba587e026690a3ace802898a0b4
Aidar Kudabaev <a.kudabaev@calendaria.ai>
2024-07-17 10:23:23 +0500

    added permision

--- ent/migrate/migrations/20240715172212_init.sql
+++ ent/migrate/migrations/20240715172212_init.sql
@@ -3,10 +3,14 @@ INSERT INTO "permission_groups" ("id", "name", "app_id")
 VALUES ('project.comment', 'Project Comment', 'common');
 
 INSERT INTO "permissions" ("id", "group_id", "name", "description", "app_id", "fields")
-VALUES ('project.comment.create', 'project.project', 'Comment Create', '', 'common', '[]'),
-       ('project.comment.read', 'project.project', 'Comment Read', '', 'common', '[]'),
--- By default people can delete and update only there own comments, except tenant owner,
--- who can edit someone else's comments
+VALUES ('project.task.create', 'project.task', 'Task Create', '', 'common', '[]'),
+       ('project.task.read', 'project.task', 'Task Read', '', 'common', '[]'),
+       ('project.task.update', 'project.task', 'Task Update', '', 'common', '[]'),
+       ('project.task.delete', 'project.task', 'Task Delete', '', 'common', '[]'),
+       ('project.comment.create', 'project.comment', 'Comment Create', '', 'common', '[]'),
+       ('project.comment.read', 'project.comment', 'Comment Read', '', 'common', '[]'),
+-- By default people can delete and update only there own comments,
+-- except tenant owner, who can edit someone else's comments and delete them
 -- and project manager, who can delete comments
        ('project.comment.update', 'project.project', 'Comment Update', '', 'common', '[]'),
        ('project.comment.delete', 'project.project', 'Comment Delete', '', 'common', '[]');
@@ -15,17 +19,28 @@ VALUES ('project.comment.create', 'project.project', 'Comment Create', '', 'comm
 INSERT INTO "role_permissions" ("role_id", "permission_id", "deny", "fields")
 VALUES
 -- Tenant admin role
+(1, 'project.task.create', FALSE, '[]'),
+(1, 'project.task.read', FALSE, '[]'),
+(1, 'project.task.update', FALSE, '[]'),
+(1, 'project.task.delete', FALSE, '[]'),
 (1, 'project.comment.create', FALSE, '[]'),
 (1, 'project.comment.read', FALSE, '[]'),
 (1, 'project.comment.update', FALSE, '[]'),
 (1, 'project.comment.delete', FALSE, '[]'),
 -- Project manager role
+(3, 'project.task.read', FALSE, '[]'),
+(3, 'project.task.update', FALSE, '[]'),
+(3, 'project.task.delete', FALSE, '[]'),
 (3, 'project.comment.create', FALSE, '[]'),
 (3, 'project.comment.read', FALSE, '[]'),
 (3, 'project.comment.delete', FALSE, '[]'),
 -- Project participant limitations
+(4, 'project.task.read', FALSE, '[]'),
+(4, 'project.task.update', FALSE, '[]'),
+(4, 'project.task.delete', FALSE, '[]'),
 (4, 'project.comment.create', FALSE, '[]'),
 (4, 'project.comment.read', FALSE, '[]'),
 -- Project reporter limitations
+(5, 'project.task.read', FALSE, '[]'),
 (5, 'project.comment.create', FALSE, '[]'),
 (5, 'project.comment.read', FALSE, '[]');
