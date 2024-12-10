2620b9e41cff25cf0c5e6af2ee24b1bbda25ebe4
Aidar Kudabaev <a.kudabaev@calendaria.ai>
2024-12-10 10:44:47 +0500

    Revert "added basic role to all tenants"
    This reverts commit 3612123c0442d7b8bd3bc5e33027bd906e9352ed.

--- ent/migrate/migrations/20241210053442_init.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO resource_accesses(tenant_id, resource_id, identity_id, role_id, resource_type)
-SELECT tenant_id, null, '', 7, null
-FROM resource_accesses
-GROUP BY tenant_id
-HAVING SUM(CASE WHEN role_id = 7 THEN 1 ELSE 0 END) = 0;
